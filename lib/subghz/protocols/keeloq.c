df7cc6ab502c5b8ec288961171eb42bcc20a6ec5
r3df0xx <daniel@dogaru.org>
2022-06-16 21:58:13 +0300

    Update keeloq.c

--- lib/subghz/protocols/keeloq.c
+++ lib/subghz/protocols/keeloq.c
@@ -658,6 +658,8 @@ if(strcmp(mfname, "") == 0) {
                 break;
             }
         }
+    } else if (strcmp(mfname, "Unknown") == 0) {
+        return 1; 
     } else {
 
     for
@@ -798,6 +800,7 @@ if(strcmp(mfname, "") == 0) {
     }
 
     *manufacture_name = "Unknown";
+    mfname = "Unknown";
     instance->cnt = 0;
 
     return 0;
